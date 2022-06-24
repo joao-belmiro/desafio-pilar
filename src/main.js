fa8573b8554a69cee74dd8833ad9d1d20b9ae35d
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2022-06-24 00:08:24 -0300

    creating global filter currency

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('formatcurrecy', function (value) {
+  return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
